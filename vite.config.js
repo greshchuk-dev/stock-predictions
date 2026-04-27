f7dd9dc230aa41a0c21481e5c442ef38ac71f4bd
Iryna Greshchuk <100215170+greshchuk-dev@users.noreply.github.com>
2026-04-27 09:19:09 +0100

    Add Remix plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import {defineConfig} from 'vite'
-
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: []	
-})
+  plugins: [
+    remix(),
+  ],
+});
